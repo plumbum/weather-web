d3274dc04373de955b5ae32f04c56ee2aaa72357
Ivan A-R <ivan@tuxotronic.org>
2016-06-06 01:16:21 +0300

    Initial commit. test bme280

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # weather-web
+
+Sample output at http://102g.ru/bme280
+
+## Hardware
+
+  * BME280
+  * bmp180 - simplest. Only pressure and themperature
---
+++ main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"github.com/davecheney/i2c"
+	"github.com/gin-gonic/gin"
+	"github.com/quinte17/bme280"
+	"log"
+	"time"
+)
+
+type MyData struct {
+	bme280.Envdata
+	Bar float64
+	Timestamp time.Time
+}
+
+var data MyData
+
+func main() {
+
+	dev, err := i2c.New(0x76, 0)
+	if err != nil {
+		log.Print(err)
+	}
+
+	bme, err := bme280.NewI2CDriver(dev)
+	if err != nil {
+		log.Print(err)
+	}
+
+	tmr := time.NewTimer(time.Second * 3)
+
+	go func() {
+		for t := range tmr.C {
+			env, err := bme.Readenv()
+			if err != nil {
+				log.Print("[ERROR] %v", err)
+				continue
+			}
+			data.Envdata = env
+			data.Bar = env.Press / 1.333224
+			data.Timestamp = t
+			log.Print(data)
+		}
+	}()
+
+	r := gin.Default()
+
+	r.GET("/bme280", func(c *gin.Context) {
+		c.Header("Refresh", "5")
+		c.JSON(200, data)
+	})
+
+	log.Print("Listen at :8280")
+	r.Run(":8280")
+}
